7bbf956c61a6fe16a24e7de2daae4b54f1400488
Steven Lehar <slehar@gmail.com>
2016-06-01 10:23:10 -0400

    Initial commit

---
+++ HarmonicGestalt.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+HarmonicGestalt.py
+
+Created on Wed Jun  1 09:45:43 2016
+
+@author: slehar
+"""
+
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+#import matplotlib.lines as mlines
+from matplotlib import animation
+#from matplotlib.widgets import CheckButtons
+#from vertslider import VertSlider
+
+ptList = []
+
+# Open figure and set axes 1 for drawing Artists
+plt.close('all')
+fig = plt.figure(figsize=(10,10))
+fig.canvas.set_window_title('Harmonic Gestalt')
+fig.text(.35, .92, 'Harmonic Gestalt', size=24)
+ax = fig.add_axes([.1, .1, .8, .8])
+ax.set_xticks([])
+ax.set_yticks([])
+
+ptList.append(mpatches.Circle((.5,.5),.02))
+ax.add_patch(ptList[-1])
+
+# The animation function (evaluated repeatedly and endlessly)
+#def animate(num):
+#    pass
+
+
+# Matplotlib animation funcion calls animate()    
+#anim = animation.FuncAnimation(fig, animate, 
+#                               repeat=True,
+#                               interval=0)
+
+# Show plot
+plt.show()
+figmgr=plt.get_current_fig_manager()
+figmgr.canvas.manager.window.raise_()
+geom=figmgr.window.geometry()
+(xLoc,yLoc,dxWidth,dyHeight)=geom.getRect()
+figmgr.window.setGeometry(10,10,dxWidth,dyHeight)
---
+++ README.txt
@@ -0,0 +1,6 @@
+HarmonicGestalt README
+
+This is an interactive Matplotlib program that demonstrates 
+harmonic resonance as a foundational principle of Gestalt phenomena.
+
+
