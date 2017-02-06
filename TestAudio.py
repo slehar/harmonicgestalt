512c4381db59828df7159c4421af201628f2af94
Steven Lehar <slehar@gmail.com>
2017-02-06 14:49:41 -0500

    fix mysterious bug now really working

--- TestAudio.py
+++ TestAudio.py
@@ -27,7 +27,8 @@ freq = 0.
 # PyAudio Callback - gets called repeatedly
 def paCallback(in_data, frame_count, time_info, status):
     global data
-    plotLines[0].set_ydata(data[:plotWidth])
+    plotLines1[0].set_ydata(data[:plotWidth])
+    plotLines2[0].set_ydata(data[RATE-plotWidth:RATE])
     return (data, pyaudio.paContinue)
 
 # PyAudio open audio stream
@@ -40,7 +41,6 @@ stream = pa.open(
             stream_callback=paCallback,
             frames_per_buffer=CHUNK)
 
-
 fData = np.zeros(RATE, dtype=float)
 for freq in [300, 400, 500]:
     fData += np.sin(time*freq)
@@ -52,16 +52,24 @@ data = np.uint8(fData)
 
 ####### Open figure and set axes 1 for drawing Artists ########
 plt.close('all')
-fig = plt.figure(figsize=(10,8))
+fig = plt.figure(figsize=(12,8))
 aspect = 10./8.
 fig.canvas.set_window_title('Test Audio')
 
-#### Main axes ####
-ax = fig.add_axes([.1, .225, .7, .75])
-ax.set_xlim([0., plotWidth])
-ax.set_ylim([0,255])
-#ax.set_xticks([])
-#ax.set_yticks([])
+#### Axes 1 ####
+ax1 = fig.add_axes([.05,.4,.4,.2])
+ax1.set_xlim([0., plotWidth])
+ax1.set_ylim([0,255])
+plt.sca(ax1)
+plotLines1 = plt.plot(data[:plotWidth])
+
+#### Axes 2 ####
+ax2 = fig.add_axes([.55,.4,.4,.2])
+ax2.set_xlim([RATE-plotWidth, RATE])
+ax2.set_ylim([0,255])
+plt.sca(ax2)
+plotLines2 = plt.plot(data[RATE-plotWidth:RATE])
+
 
 # Keypress 'q' to quit
 def press(event):
@@ -73,33 +81,32 @@ def press(event):
         plt.close()
 fig.canvas.mpl_connect('key_press_event', press)
 
-plt.ion()
-plotLines = plt.plot(data[:plotWidth])
+#plt.ion()
 
 #  frequency slider
-axSlider1 = fig.add_axes([0.3, 0.125, 0.234, 0.04])
-axSlider1.set_xticks([])
-axSlider1.set_yticks([])
-slider1 = Slider(axSlider1, 'frequency', 100, 1000., valinit=300.)
-freq = slider1.val
-
-def update1(val):
-    global freq, data, fData, time
-    freq = slider1.val
-    fData = np.zeros(RATE, dtype=float)
-    fData += np.sin(time*freq)
-    fData = fData / np.max(np.abs(fData)) * 127 + 128
-    lastTime = time[-1] + (time[-1] - time[-2])
-    time = np.linspace(lastTime, lastTime+twoPi, RATE)
-    plt.sca(ax)
-    data = np.uint8(fData)
-    plt.pause(.001)
-    
-slider1.on_changed(update1)
+#axSlider1 = fig.add_axes([0.3, 0.125, 0.234, 0.04])
+#axSlider1.set_xticks([])
+#axSlider1.set_yticks([])
+#slider1 = Slider(axSlider1, 'frequency', 100, 1000., valinit=300.)
+#freq = slider1.val
+#
+#def update1(val):
+#    global freq, data, fData, time
+#    freq = slider1.val
+#    fData = np.zeros(RATE, dtype=float)
+#    fData += np.sin(time*freq)
+#    fData = fData / np.max(np.abs(fData)) * 127 + 128
+#    lastTime = time[-1] + (time[-1] - time[-2])
+#    time = np.linspace(lastTime, lastTime+twoPi, RATE)
+#    plt.sca(ax)
+#    data = np.uint8(fData)
+#    plt.pause(.001)
+#    
+#slider1.on_changed(update1)
 
 
 # start the stream (4)
-stream.start_stream()
+#stream.start_stream()
 
 # Show plot
 plt.show()
