437d0f2421894af8429f8d6cd5ba0ba7405cb499
Steven Lehar <slehar@gmail.com>
2017-02-28 20:08:33 -0500

    rescaled spectrum

--- HarmonicGestalt.py
+++ HarmonicGestalt.py
@@ -25,7 +25,7 @@ data  = np.zeros(CHUNK, dtype=float)     # buffer of data
 time  = np.linspace(0, twoPi, CHUNK)     # time of data
 fData = np.sin(time)
 plotTime = np.arange(0, twoPi, twoPi/PLOTWIDTH)
-freqMin, freqMax = -4.1, 4.1
+freqMin, freqMax = -2.1, 2.1
 freqRange = freqMax - freqMin
 plotFreq = np.arange(freqMin, freqMax, freqRange/PLOTWIDTH)
 plotData = np.zeros_like(plotTime)
